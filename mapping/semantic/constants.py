db869808fa127ef80b5f93c86776af0808edde69
jiaming <jamie-w@nus.edu.sg>
2023-07-05 16:11:23 +0800

    del unnecessary files

--- mapping/semantic/constants.py
+++ mapping/semantic/constants.py
@@ -7,10 +7,10 @@ class MapConstants:
     BEEN_CLOSE_MAP = 4
     PROBABILITY_MAP = 5 # Probability of goal object being at location (i, j)
 
-class ProbabilisticMapConstants:
-    NON_SEM_CHANNELS = 5  # Number of non-semantic channels at the start of maps
-    OBSTACLE_MAP = 0
-    EXPLORED_MAP = 1
-    CURRENT_LOCATION = 2
-    VISITED_MAP = 3
-    BEEN_CLOSE_MAP = 4
+# class ProbabilisticMapConstants:
+#     NON_SEM_CHANNELS = 5  # Number of non-semantic channels at the start of maps
+#     OBSTACLE_MAP = 0
+#     EXPLORED_MAP = 1
+#     CURRENT_LOCATION = 2
+#     VISITED_MAP = 3
+#     BEEN_CLOSE_MAP = 4
